9b6c9d94c75afa22b80f76743d6c7d3c865b4e27
Aissam4 <aissambarchil75@gmail.com>
2022-03-24 23:56:42 +0100

    add module_01 directoryse

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: abarchil <abarchil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 15:16:43 by abarchil          #+#    #+#             */
-/*   Updated: 2022/03/22 01:20:55 by abarchil         ###   ########.fr       */
+/*   Updated: 2022/03/23 15:15:08 by abarchil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@
 int main(int ac, char **av)
 {
 	if (ac == 1)
-        return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *", 1);
+        return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 1);
 	for (int i = 1; i < ac; i++)
 		for(size_t j = 0; j < strlen(av[i]); j++)
 			std::cout << (av[i][j] = toupper(av[i][j]));
+	std::cout << std::endl;
 	return (0);
 }
