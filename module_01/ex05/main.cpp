7f523bd123226b89d3a2880a5b103cc4d319bc8c
Aissam4 <aissambarchil75@gmail.com>
2022-03-27 01:14:26 +0100

    (linux)add ex05 module01

--- module_01/ex05/main.cpp
+++ module_01/ex05/main.cpp
@@ -1,10 +1,9 @@
 #include "harl.hpp"
 
-int main(int ac, char **av)
+int main( void )
 {
     Harl child;
-    (void)ac;
-    if (ac != 2)
-        return (std::cout << "Argument Error" << std::endl, 1);
-    child.complain(av[1]);
+    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for (int i = 0; i < 4; i++)
+        std::cout << "=> ", child.complain(levels[i]);
 }
