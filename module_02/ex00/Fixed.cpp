10b549472636bfa55e1113d7123e1d321694d8ff
Aissam4 <aissambarchil75@gmail.com>
2022-03-29 16:56:40 +0000

    (ex00 module02)finish

--- module_02/ex00/Fixed.cpp
+++ module_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: abarchil <abarchil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 15:20:39 by abarchil          #+#    #+#             */
-/*   Updated: 2022/03/29 16:43:16 by abarchil         ###   ########.fr       */
+/*   Updated: 2022/03/29 16:55:17 by abarchil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,24 @@ void	Fixed::setFixedPoint(int	FixedPoint)
 
 int		Fixed::getRawBits() const
 {
+	std::cout << "getRawBits member function called" << std::endl;
 	return this->_FixedPointNum;
 }
 
 Fixed::Fixed()
 {
+	std::cout << "Default constructor called" << std::endl;
 	this->_FixedPointNum = 0;
 }
 
 Fixed::~Fixed( void )
 {
-	std::cout << "destructor called" << std::endl;
+	std::cout << "Destructor called" << std::endl;
 }
 
 Fixed	& Fixed::operator=(const Fixed &obj)
 {
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this == &obj)
 		return *this;
 	else
@@ -43,5 +46,6 @@ Fixed	& Fixed::operator=(const Fixed &obj)
 
 Fixed::Fixed(const Fixed &obj)
 {
+	std::cout << "Copy constructor called" << std::endl;
 	this->_FixedPointNum = obj.getRawBits();
 }
