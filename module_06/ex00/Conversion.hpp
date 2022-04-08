4ce4b082bfb85e042af4f040783a17e2b6791036
Aissam Barchil <abarchil@e1r9p6.1337.ma>
2022-04-08 09:24:02 +0000

    (module_06 ex00) update the ex00 (manage files)

--- module_06/ex00/Conversion.hpp
+++ module_06/ex00/Conversion.hpp
@@ -6,12 +6,21 @@
 /*   By: abarchil <abarchil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 20:31:08 by abarchil          #+#    #+#             */
-/*   Updated: 2022/04/07 20:33:58 by abarchil         ###   ########.fr       */
+/*   Updated: 2022/04/08 09:22:58 by abarchil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __CONVERSION_HPP__
 #define __CONVERSION_HPP__
 # include <iostream>
+#include <typeinfo>
+#include <string>
+bool    check_if_valid(std::string &str);
+bool    checkPseudo(std::string &str);
+void    TooDouble(std::string str);
+int     CheckZeros(std::string str, size_t index);
 
+void    TooFloat(std::string str);
+void    TooChar(std::string &str);
+void    TooInt(std::string &str);
 #endif
